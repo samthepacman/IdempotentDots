563d3210e74d1a8d9ed85230615e1d666ffe0b99
Chandra <68412503+Sam1431@users.noreply.github.com>
2022-01-14 12:16:08 +0530

    Update lightspeed.lua

--- home/.config/nvim/lua/plugins/lightspeed.lua
+++ home/.config/nvim/lua/plugins/lightspeed.lua
@@ -1,9 +1,7 @@
 require'lightspeed'.setup {
-  jump_on_partial_input_safety_timeout = 400,
+  jump_to_unique_chars = 400,
   -- This can get _really_ slow if the window has a lot of content,
   -- turn it on only if your machine can always cope with it.
-  highlight_unique_chars = false,
-  grey_out_search_area = true,
   match_only_the_start_of_same_char_seqs = true,
   limit_ft_matches = 10,
   -- Characters to be substituted in the highlighted matches.
